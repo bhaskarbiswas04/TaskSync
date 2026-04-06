9dc4f18b21c6a84556dd8275232f081f7d3b04c5
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-07 01:31:55 +0530

    updt: fe & be init

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/config/server.js";
+
+dotenv.config(); //--load env variables
+
+connectDB(); //--connect database
+
+const app = express(); 
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+//Testing route
+app.get("/", (req, res)=>{
+    res.send("API is running smoothly...");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
+
