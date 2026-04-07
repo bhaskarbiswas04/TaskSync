6d5faa3eb6e46e9cf8ad2fc42c41801b79e6d117
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-08 02:27:33 +0530

    updt: user login endpoint

---
+++ backend/src/middleware/isAuthenticated.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken"
+import { User } from "../models/User.model"
+
+const isAuthenticated = async (req, res, next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
