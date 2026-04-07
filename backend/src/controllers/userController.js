ff7b44f17a8cdd846e5bcc691de22cd4feb1da9f
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-08 02:15:20 +0530

    updt: user login endpoint

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -101,7 +101,7 @@ export const login = async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: "Server error",
+      message: "Server Error",
     });
   }
 };
