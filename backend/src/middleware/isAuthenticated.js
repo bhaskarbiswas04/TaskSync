388fa69cc0fd27bd81e32f8b9693f498ca8af67a
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-08 09:45:40 +0530

    updt: isAuthenticate middleware impl

--- backend/src/middleware/isAuthenticated.js
+++ backend/src/middleware/isAuthenticated.js
@@ -1,10 +1,37 @@
 import jwt from "jsonwebtoken"
-import { User } from "../models/User.model"
+import { User } from "../models/User.model.js"
 
 const isAuthenticated = async (req, res, next)=>{
     try {
-        
+        let token;
+
+        //-get token from header
+        if (
+          req.headers.authorization &&
+          req.headers.authorization.startsWith("Bearer")
+        ) {
+          token = req.headers.authorization.split(" ")[1];
+        }
+
+        if (!token) {
+          return res.status(401).json({
+            message: "Not authorized, no token",
+          });
+        }
+
+        //-verify token
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        console.log("DECODED:", decoded);
+
+        //-Get user from the token.
+        req.user = await User.findById(decoded.userId).select("-password");
+
+        next();
     } catch (error) {
-        
+        return res.status(401).json({
+          message: "Not authorized, invalid token",
+        });
     }
 }
+
+export default isAuthenticated;
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { signup, login } from "../controllers/userController.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/me", isAuthenticated, (req, res) => {
+  res.json({
+    message: "Access granted.",
+    user: req.user,
+  });
+});
 
 export default router;
