9ce123094e5f7e6c2753075f3208b06962cbb92c
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-10 04:53:13 +0530

    updt: cors policy

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,14 @@ dotenv.config(); //--load env variables
 
 const app = express(); 
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://task-sync-client.vercel.app/"],
+  credentials: true,
+};
+
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000; 
 
