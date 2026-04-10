2e2cea129f435110e3ea9faddf8963e76a6f8e95
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-10 18:07:34 +0530

    updt: getAllTeams route

--- backend/src/routes/teamRoutes.js
+++ backend/src/routes/teamRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createTeam, getTeams } from "../controllers/teamController.js";
+import { createTeam, getTeams, getAllTeams } from "../controllers/teamController.js";
 import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
 router.route("/").post(isAuthenticated, createTeam);
 router.route("/").get(isAuthenticated, getTeams);
+router.route("/all-teams").get(isAuthenticated, getAllTeams);
 
 export default router;
