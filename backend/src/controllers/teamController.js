5c4e4bba1d2099819380c02c8417636130a5c0e1
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-10 18:06:56 +0530

    updt: get all teams route

--- backend/src/controllers/teamController.js
+++ backend/src/controllers/teamController.js
@@ -45,3 +45,28 @@ export const getTeams = async (req, res)=>{
         });
     }
 }
+
+//--RouteLogic: GET- all Teams
+export const getAllTeams = async (req, res) => {
+  try {
+    const teams = await Team.find()
+      .populate("members", "name email avatar")
+
+    //-Validation
+    if (!teams || teams.length === 0) {
+      return res.status(404).json({ message: "No teams found" });
+    }
+
+    return res.status(200).json({
+      success: true,
+      count: teams.length,
+      teams,
+    });
+  } catch (error) {
+    console.error("Error fetching teams:", error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server error while fetching teams",
+    });
+  }
+};
