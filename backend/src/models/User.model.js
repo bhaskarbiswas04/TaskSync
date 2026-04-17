39c3bb3e44723a4d539f0b2b18d7435a9946c145
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-17 18:34:53 +0530

    updt: user model updated to accept setting logic too

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -4,6 +4,24 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true }, // User's name
   email: { type: String, required: true, unique: true }, // Email must be unique
   password: { type: String, required: true }, // Email must be unique
+
+  preferences: {
+    defaultPriority: {
+      type: String,
+      enum: ["Low", "Medium", "High"],
+      default: "Medium",
+    },
+    defaultStatus: {
+      type: String,
+      enum: ["To Do", "In Progress", "Completed"],
+      default: "To Do",
+    },
+    theme: {
+      type: String,
+      enum: ["light", "dark"],
+      default: "dark",
+    },
+  },
 });
 
 export const User = mongoose.model("User", userSchema);
