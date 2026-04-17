0978369e831d607aa56c4387b2257179c9ef9b55
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-17 18:32:37 +0530

    updt: setting routes created

---
+++ backend/src/routes/settingRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  updateProfile,
+  changePassword,
+  updatePreferences,
+  deleteAccount,
+} from "../controllers/user.controller.js";
+import isAuthenticated from "../middleware/isAuthenticated.js" 
+
+const router = express.Router();
+
+router.post("/profile", isAuthenticated, updateProfile);
+router.post("/password", isAuthenticated, changePassword);
+router.post("/preferences", isAuthenticated, updatePreferences);
+router.delete("/delete", isAuthenticated, deleteAccount);
+
+export default router;
