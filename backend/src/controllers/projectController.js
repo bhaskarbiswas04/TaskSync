cc088f720d7ca2617616e7cb85ef1cb4ad991546
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-21 16:38:16 +0530

    updt: removed team

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,10 +1,9 @@
 import { Project } from "../models/Project.model.js";
-import { Team } from "../models/Team.model.js";
 
 //--RouteLogic: POST- Create New Project.
 export const createProject = async (req, res) => {
   try {
-    const { name, description, team } = req.body;
+    const { name, description } = req.body;
 
     //-Validations
     if (!name) {
@@ -17,31 +16,29 @@ export const createProject = async (req, res) => {
       createdBy: req.user._id,
     });
 
-    res.status(201).json({ message: "Project created successfully:", project });
+    return res.status(201).json({
+      message: "Project created successfully",
+      project,
+    });
   } catch (error) {
-    res.status(500).json({ message: "Error creating project" });
+    return res.status(500).json({
+      message: "Error creating project",
+    });
   }
 };
 
-//--RouteLogic: GET- user-based filtering (projects where user belongs to team)
+//--RouteLogic: GET- Fetch all projects created by user
 export const getProjects = async (req, res) => {
   try {
-    // Get teams where user belongs
-    const teams = await Team.find({
-      $or: [{ createdBy: req.user._id }, { members: req.user._id }],
-    }).select("_id");
-
-    const teamIds = teams.map((t) => t._id);
-
     const projects = await Project.find({
-      team: { $in: teamIds },
+      createdBy: req.user._id,
     })
-      .populate("team", "name")
-      .populate("createdBy", "name email");
+      .populate("createdBy", "name email")
+      .sort({ createdAt: -1 });
 
-    res.json(projects);
+    return res.json(projects);
   } catch (error) {
-    res.status(500).json({
+    return res.status(500).json({
       message: "Error fetching projects",
     });
   }
