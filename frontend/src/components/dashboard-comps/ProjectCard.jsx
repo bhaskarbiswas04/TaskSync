b4bffc0e944a62b9d717df8b4891e5b22bf8749c
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-22 10:42:18 +0530

    updt: improvised

--- frontend/src/components/dashboard-comps/ProjectCard.jsx
+++ frontend/src/components/dashboard-comps/ProjectCard.jsx
@@ -10,7 +10,7 @@ export default function ProjectCard({ project }) {
     >
       <h3 className="text-lg font-semibold mt-2 text-white">{project.name}</h3>
 
-      <p className="text-sm text-cyan-400 mt-1">Description: {project.description}</p>
+      <p className="text-sm text-cyan-400 mt-1">Description: {project.description ? project.description : "N/A"}</p>
 
       {/* SAFE TEAM DISPLAY */}
       {/* <p className="text-sm text-cyan-400 mt-1">
