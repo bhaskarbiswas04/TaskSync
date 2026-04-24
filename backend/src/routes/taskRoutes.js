3f2d1432f977b121f5fbff901932ffff57fe0381
BHASKAR BISWAS <73770735+bhaskarbiswas04@users.noreply.github.com>
2026-04-24 10:30:27 +0530

    updt: task route updated

--- backend/src/routes/taskRoutes.js
+++ backend/src/routes/taskRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated.js";
-import { createTask, getTasks, updateTaskById, deleteTask } from "../controllers/taskController.js";
+import { createTask, getTasks, updateTaskById, deleteTask, getTaskById } from "../controllers/taskController.js";
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.route("/").post(isAuthenticated, createTask);
 router.route("/").get(isAuthenticated, getTasks);
 router.route("/:id").post(isAuthenticated, updateTaskById);
 router.route("/:id").delete(isAuthenticated, deleteTask);
+router.route("/:id").get(isAuthenticated, getTaskById);
 
 export default router;
